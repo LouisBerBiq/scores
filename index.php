6a3341a71343f27143a06c3703daee5d6399325c
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-03-25 16:19:43 +0100

    added matches array combination

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-include('./vue.php');
+define('MATCHES_FILE_PATH', './matches.csv');
+
+$matches = [];
+$matchesHandle = fopen(MATCHES_FILE_PATH, 'r');
+$tableHeaders = fgetcsv($matchesHandle, 1000, ',');
+while($tableLine = fgetcsv($matchesHandle, 1000, ',')) {
+	$matches[] = array_combine($tableHeaders, $tableLine);
+};
+var_dump($matches); exit();
+
+include('./vue.php'); 
