c2f4b6037abc9015256f6680b1f12bbf8b6f84d5
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-04-01 14:14:13 +0200

    added empty array init

--- index.php
+++ index.php
@@ -2,11 +2,38 @@
 
 define('MATCHES_FILE_PATH', './matches.csv');
 
+// final arrays
 $matches = [];
+$standing = [];
+
+$teams = [];
+
+function initEmptyStatsArray() {
+	return [
+		'games' => 0,
+		'score' => 0,
+		'wins' => 0,
+		'losses' => 0,
+		'draws' => 0,
+		'GF' => 0,
+		'GA' => 0,
+		'GD' => 0,
+	];
+};
+
 $matchesHandle = fopen(MATCHES_FILE_PATH, 'r');
 $tableHeaders = fgetcsv($matchesHandle, 1000, ',');
 while($tableLine = fgetcsv($matchesHandle, 1000, ',')) {
-	$matches[] = array_combine($tableHeaders, $tableLine);
+	$match = array_combine($tableHeaders, $tableLine);
+	$matches[] = $match;
+	$homeTeam = $match['home-team'];
+	$awayTeam = $match['away-team'];
+	if(!array_key_exists($homeTeam, $standing)) {
+		$standing[$homeTeam] = initEmptyStatsArray(); // create index $hometeam and fill it this array
+	}
+	if(!array_key_exists($awayTeam, $standing)) {
+		$standing[$awayTeam] = initEmptyStatsArray();
+	}
 };
 var_dump($matches); exit();
 
