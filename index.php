401d02e64d6579691b06bdf1d7d01daf411ff7d5
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-04-01 14:32:55 +0200

    spacing

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ $standing = [];
 
 $teams = [];
 
-function initEmptyStatsArray() {
+function initEmptyStatsArray()
+{
 	return [
 		'games' => 0,
 		'score' => 0,
@@ -24,18 +25,18 @@ function initEmptyStatsArray() {
 
 $matchesHandle = fopen(MATCHES_FILE_PATH, 'r');
 $tableHeaders = fgetcsv($matchesHandle, 1000, ',');
-while($tableLine = fgetcsv($matchesHandle, 1000, ',')) {
+
+while ($tableLine = fgetcsv($matchesHandle, 1000, ',')) {
 	$match = array_combine($tableHeaders, $tableLine);
-	$matches[] = $match;
+	$matches[] = $match	;
 	$homeTeam = $match['home-team'];
 	$awayTeam = $match['away-team'];
-	if(!array_key_exists($homeTeam, $standing)) {
+	if (!array_key_exists($homeTeam, $standing)) {
 		$standing[$homeTeam] = initEmptyStatsArray(); // create index $hometeam and fill it this array
 	}
-	if(!array_key_exists($awayTeam, $standing)) {
+	if (!array_key_exists($awayTeam, $standing)) {
 		$standing[$awayTeam] = initEmptyStatsArray();
 	}
 };
-var_dump( $standing); exit();
 
-include('./vue.php'); 
+include('./vue.php');
