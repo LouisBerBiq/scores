896cce6aeef7d09eaf1003bd9859dd4044049ff4
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-16 18:31:50 +0200

    Revert "Removed comment out old code"
    This reverts commit 3d27cab6ca94a6bc0df80d865be1db9ef9508c1b.

--- index.php
+++ index.php
@@ -39,4 +39,60 @@ function initEmptyStatsArray()
 	];
 };
 
+// $matchesHandle = fopen(MATCHES_FILE_PATH, 'r');
+// $tableHeaders = fgetcsv($matchesHandle, 1000, ',');
+
+// while ($tableLine = fgetcsv($matchesHandle, 1000, ',')) {
+// 	$match = array_combine($tableHeaders, $tableLine); // ceci est le match courant
+// 	$matches[] = $match;
+// 	$homeTeam = $match['home-team'];
+// 	$awayTeam = $match['away-team'];
+// 	if (!array_key_exists($homeTeam, $standing)) {
+// 		$standing[$homeTeam] = initEmptyStatsArray(); // create index $hometeam and fill it this array
+// 	};
+// 	if (!array_key_exists($awayTeam, $standing)) {
+// 		$standing[$awayTeam] = initEmptyStatsArray();
+// 	};
+// 	$standing[$homeTeam]['games']++;
+// 	$standing[$awayTeam]['games']++;
+
+// 	// draws //
+// 	if ($match['home-team-goals'] === $match['away-team-goals']) {
+// 		$standing[$homeTeam]['score']++;
+// 		$standing[$awayTeam]['score']++;
+// 		$standing[$homeTeam]['draws']++;
+// 		$standing[$awayTeam]['draws']++;
+
+// 		// home wins //
+// 	} elseif ($match['home-team-goals'] > $match['away-team-goals']) {
+// 		$standing[$homeTeam]['score'] += 3;
+// 		$standing[$homeTeam]['wins']++;
+// 		$standing[$awayTeam]['losses']++;
+
+// 		// home losses //
+// 	} else {
+// 		$standing[$awayTeam]['score'] += 3;
+// 		$standing[$awayTeam]['wins']++;
+// 		$standing[$homeTeam]['losses']++;
+// 	};
+
+// 	// goals comparisons //
+// 	$standing[$homeTeam]['GF'] += $match['home-team-goals'];
+// 	$standing[$awayTeam]['GF'] += $match['away-team-goals'];
+// 	$standing[$homeTeam]['GA'] += $match['away-team-goals'];
+// 	$standing[$awayTeam]['GA'] += $match['home-team-goals'];
+// 	$standing[$homeTeam]['GD'] = $standing[$homeTeam]['GF'] - $standing[$homeTeam]['GA'];
+// 	$standing[$awayTeam]['GD'] = $standing[$awayTeam]['GF'] - $standing[$awayTeam]['GA'];
+// };
+
+// uasort($standing, function ($a, $b) {
+// 	if ($a['score'] === $b['score']) {
+// 		return 0;
+// 	};
+// 	return $a['score'] > $b['score'] ? -1 : 1;
+// });
+
+$teams = array_keys($standing);
+sort($teams);
+
 include('./views/vue.php');
