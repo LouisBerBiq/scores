5609bed9234846ce8c61dd64aa3407b174bd7e66
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-17 19:35:34 +0200

    Moved empty standings initialization into its own file

--- index.php
+++ index.php
@@ -14,22 +14,8 @@ $connection = getConnection();
 $teams = allTeams($connection);
 $matches = allWithTeamsGrouped(allMatchesWithTeams($connection));
 
+
 $standing = [];
-function initEmptyStatsArray()
-{
-	return [
-		'games' => 0,
-		'score' => 0,
-		'wins' => 0,
-		'losses' => 0,
-		'draws' => 0,
-		'GF' => 0,
-		'GA' => 0,
-		'GD' => 0,
-	];
-};
-
-
 foreach ($matches as $match){
 	$homeTeam = $match->home_team;
 	$awayTeam = $match->away_team;
---
+++ utils/standings.php
@@ -0,0 +1,14 @@
+<?php
+function initEmptyStatsArray()
+{
+	return [
+		'games' => 0,
+		'score' => 0,
+		'wins' => 0,
+		'losses' => 0,
+		'draws' => 0,
+		'GF' => 0,
+		'GA' => 0,
+		'GD' => 0,
+	];
+};
