00414858cd76e9d450a94f40cfa338797c918f09
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-18 03:13:55 +0200

    moved "standing making" into it's own request branch (get)

--- index.php
+++ index.php
@@ -29,57 +29,59 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 		}
 	};
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET'){
-$standing = [];
-foreach ($matches as $match){
-	$homeTeam = $match->home_team;
-	$awayTeam = $match->away_team;
+	if (!isset($_GET['action']) && !isset($_GET['resource'])){
+	$standing = [];
+	foreach ($matches as $match){
+		$homeTeam = $match->home_team;
+		$awayTeam = $match->away_team;
 
-	if (!array_key_exists($homeTeam, $standing)) {
-		$standing[$homeTeam] = initEmptyStatsArray(); // create index $hometeam and fill it this array
-	};
-	if (!array_key_exists($awayTeam, $standing)) {
-		$standing[$awayTeam] = initEmptyStatsArray();
-	};
-	$standing[$homeTeam]['games']++;
-	$standing[$awayTeam]['games']++;
+		if (!array_key_exists($homeTeam, $standing)) {
+			$standing[$homeTeam] = initEmptyStatsArray(); // create index $hometeam and fill it this array
+		};
+		if (!array_key_exists($awayTeam, $standing)) {
+			$standing[$awayTeam] = initEmptyStatsArray();
+		};
+		$standing[$homeTeam]['games']++;
+		$standing[$awayTeam]['games']++;
 
-	// draws //
-	if ($match->home_team_goals === $match->away_team_goals) {
-		$standing[$homeTeam]['score']++;
-		$standing[$awayTeam]['score']++;
-		$standing[$homeTeam]['draws']++;
-		$standing[$awayTeam]['draws']++;
+		// draws //
+		if ($match->home_team_goals === $match->away_team_goals) {
+			$standing[$homeTeam]['score']++;
+			$standing[$awayTeam]['score']++;
+			$standing[$homeTeam]['draws']++;
+			$standing[$awayTeam]['draws']++;
 
-		// home wins //
-	} elseif ($match->home_team_goals > $match->away_team_goals) {
-		$standing[$homeTeam]['score'] += 3;
-		$standing[$homeTeam]['wins']++;
-		$standing[$awayTeam]['losses']++;
+			// home wins //
+		} elseif ($match->home_team_goals > $match->away_team_goals) {
+			$standing[$homeTeam]['score'] += 3;
+			$standing[$homeTeam]['wins']++;
+			$standing[$awayTeam]['losses']++;
 
-		// home losses //
-	} else {
-		$standing[$awayTeam]['score'] += 3;
-		$standing[$awayTeam]['wins']++;
-		$standing[$homeTeam]['losses']++;
-	};
+			// home losses //
+		} else {
+			$standing[$awayTeam]['score'] += 3;
+			$standing[$awayTeam]['wins']++;
+			$standing[$homeTeam]['losses']++;
+		};
 
-	// goals comparisons //
-	$standing[$homeTeam]['GF'] += $match->home_team_goals;
-	$standing[$awayTeam]['GF'] += $match->away_team_goals;
-	$standing[$homeTeam]['GA'] += $match->away_team_goals;
-	$standing[$awayTeam]['GA'] += $match->home_team_goals;
-	$standing[$homeTeam]['GD'] = $standing[$homeTeam]['GF'] - $standing[$homeTeam]['GA'];
-	$standing[$awayTeam]['GD'] = $standing[$awayTeam]['GF'] - $standing[$awayTeam]['GA'];
+		// goals comparisons //
+		$standing[$homeTeam]['GF'] += $match->home_team_goals;
+		$standing[$awayTeam]['GF'] += $match->away_team_goals;
+		$standing[$homeTeam]['GA'] += $match->away_team_goals;
+		$standing[$awayTeam]['GA'] += $match->home_team_goals;
+		$standing[$homeTeam]['GD'] = $standing[$homeTeam]['GF'] - $standing[$homeTeam]['GA'];
+		$standing[$awayTeam]['GD'] = $standing[$awayTeam]['GF'] - $standing[$awayTeam]['GA'];
+	}
+	uasort($standing, function ($a, $b) {
+		if ($a['score'] === $b['score']) {
+			return 0;
+		};
+		return $a['score'] > $b['score'] ? -1 : 1;
+	});
 }
-
-uasort($standing, function ($a, $b) {
-	if ($a['score'] === $b['score']) {
-		return 0;
-	};
-	return $a['score'] > $b['score'] ? -1 : 1;
-});
-
-$teams = array_keys($standing);
-sort($teams);
+} else{
+	header('locattion: ./index.php');
+	exit();
+}
 
 include('./views/vue.php');
