b2c7e7efde1f21effa091e20cc2dbb59a7f9be5e
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-19 23:27:55 +0200

    Moved dashboard stuff into page.php

---
+++ controllers/page.php
@@ -0,0 +1,63 @@
+<?php
+namespace Controllers\Page;
+
+use function Models\Match\allWithTeamsGrouped as allWithTeamsGrouped;
+use function Models\Match\allWithTeams as allMatchesWithTeams;
+use function Models\Team\all as allTeams;
+
+function dashboard(\PDO $connection)
+{
+	$matches = allWithTeamsGrouped(allMatchesWithTeams($connection));
+	$teams =  allTeams($connection);
+	$standing = [];
+
+	foreach ($matches as $match) {
+		$homeTeam = $match->home_team;
+		$awayTeam = $match->away_team;
+
+		if (!array_key_exists($homeTeam, $standing)) {
+			$standing[$homeTeam] = initEmptyStatsArray();
+		};
+		if (!array_key_exists($awayTeam, $standing)) {
+			$standing[$awayTeam] = initEmptyStatsArray();
+		};
+		$standing[$homeTeam]['games']++;
+		$standing[$awayTeam]['games']++;
+
+		// draws //
+		if ($match->home_team_goals === $match->away_team_goals) {
+			$standing[$homeTeam]['score']++;
+			$standing[$awayTeam]['score']++;
+			$standing[$homeTeam]['draws']++;
+			$standing[$awayTeam]['draws']++;
+
+			// home wins //
+		} elseif ($match->home_team_goals > $match->away_team_goals) {
+			$standing[$homeTeam]['score'] += 3;
+			$standing[$homeTeam]['wins']++;
+			$standing[$awayTeam]['losses']++;
+
+			// home losses //
+		} else {
+			$standing[$awayTeam]['score'] += 3;
+			$standing[$awayTeam]['wins']++;
+			$standing[$homeTeam]['losses']++;
+		};
+
+		// goals comparisons //
+		$standing[$homeTeam]['GF'] += $match->home_team_goals;
+		$standing[$awayTeam]['GF'] += $match->away_team_goals;
+		$standing[$homeTeam]['GA'] += $match->away_team_goals;
+		$standing[$awayTeam]['GA'] += $match->home_team_goals;
+		$standing[$homeTeam]['GD'] = $standing[$homeTeam]['GF'] - $standing[$homeTeam]['GA'];
+		$standing[$awayTeam]['GD'] = $standing[$awayTeam]['GF'] - $standing[$awayTeam]['GA'];
+	}
+	uasort($standing, static function ($a, $b) {
+		if ($a['score'] === $b['score']) {
+			return 0;
+		};
+		return $a['score'] > $b['score'] ? -1 : 1;
+	});
+
+	return compact('matches', 'teams', 'standing');
+}
--- index.php
+++ index.php
@@ -39,54 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	};
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	if (!isset($_GET['action']) && !isset($_GET['resource'])) {
-		$standing = [];
-		foreach ($matches as $match) {
-			$homeTeam = $match->home_team;
-			$awayTeam = $match->away_team;
-
-			if (!array_key_exists($homeTeam, $standing)) {
-				$standing[$homeTeam] = initEmptyStatsArray();
-			};
-			if (!array_key_exists($awayTeam, $standing)) {
-				$standing[$awayTeam] = initEmptyStatsArray();
-			};
-			$standing[$homeTeam]['games']++;
-			$standing[$awayTeam]['games']++;
-
-			// draws //
-			if ($match->home_team_goals === $match->away_team_goals) {
-				$standing[$homeTeam]['score']++;
-				$standing[$awayTeam]['score']++;
-				$standing[$homeTeam]['draws']++;
-				$standing[$awayTeam]['draws']++;
-
-				// home wins //
-			} elseif ($match->home_team_goals > $match->away_team_goals) {
-				$standing[$homeTeam]['score'] += 3;
-				$standing[$homeTeam]['wins']++;
-				$standing[$awayTeam]['losses']++;
-
-				// home losses //
-			} else {
-				$standing[$awayTeam]['score'] += 3;
-				$standing[$awayTeam]['wins']++;
-				$standing[$homeTeam]['losses']++;
-			};
-
-			// goals comparisons //
-			$standing[$homeTeam]['GF'] += $match->home_team_goals;
-			$standing[$awayTeam]['GF'] += $match->away_team_goals;
-			$standing[$homeTeam]['GA'] += $match->away_team_goals;
-			$standing[$awayTeam]['GA'] += $match->home_team_goals;
-			$standing[$homeTeam]['GD'] = $standing[$homeTeam]['GF'] - $standing[$homeTeam]['GA'];
-			$standing[$awayTeam]['GD'] = $standing[$awayTeam]['GF'] - $standing[$awayTeam]['GA'];
-		}
-		uasort($standing, function ($a, $b) {
-			if ($a['score'] === $b['score']) {
-				return 0;
-			};
-			return $a['score'] > $b['score'] ? -1 : 1;
-		});
+		$boardData = dashboard($connection);
 	}
 } else {
 	header('locattion: ./index.php');
