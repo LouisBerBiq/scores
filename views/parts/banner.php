2d67fca70354893c61ad27cc38c5e3fed0bfecdb
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-20 20:28:14 +0200

    Added reusable components

---
+++ views/parts/banner.php
@@ -0,0 +1,5 @@
+<div>
+	<a href="index.php">
+		<h1>Premier League 2020</h1>
+	</a>
+</div>
---
+++ views/parts/navigation.php
@@ -0,0 +1,3 @@
+<div role="navigation">
+	<a href="/?action=create&resource=team">Add a team</a> - <a href="/?action=create&resource=match">Add a match</a>
+</div>
