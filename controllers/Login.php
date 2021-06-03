92655f175bfaa90293f4a793bba7b5c932c16661
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-06-03 22:57:25 +0200

    Added Email validation
    and some ground work for logout

--- controllers/Login.php
+++ controllers/Login.php
@@ -10,19 +10,55 @@ class Login
 	}
 	public function check()
 	{
-		//TODO: validation
-
 		$userModel = new \Models\User();
-
+		
 		$email = $_POST['user-email'];
 		$password = $_POST['user-password'];
 
-		$user = $userModel->findByEmail($email);
-		if (password_verify($password, $user->password)) {
-			header('location: ./index.php');
-		} else {
-			header('location: ./index.php?action=show&resource=login');
-		}
+		$this->filterEmail($email);
+
+		if (!$_SESSION['errors']) {
+			$user = $userModel->findByEmail($email);
+			if (password_verify($password, $user->password)) {
+				$_SESSION['user'] = $user;
+				header('location: ./index.php');
+			} else {
+				header('location: ./index.php?action=show&resource=login');
+			}
+		} 
 		exit();
+	}
+	public function delete()
+	{
+		// will use this if there are more data to the session than errors and user
+		// unset($_SESSION['user']);
+		// otherwise, we're just using this written exemple from https://www.php.net/manual/en/function.session-destroy.php
+		
+		$_SESSION = array();
+
+		// If it's desired to kill the session, also delete the session cookie.
+		// Note: This will destroy the session, and not just the session data!
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+				$params["secure"], $params["httponly"]
+			);
+		}
+		
+		// Finally, destroy the session.
+		session_destroy();
+
+		header('location: ./index.php');
+		exit();
+	}
+	private function filterEmail($var)
+	{
+		if (filter_var($var, FILTER_SANITIZE_EMAIL)) {
+			if (filter_var($var, FILTER_VALIDATE_EMAIL)) {
+				return;
+			} 
+		}
+		$_SESSION['errors']['user-email'] = 'your email is not correct';
 	}
 }
